b878bccd55c6fbafdb67d70ebb0f44bce0eec48f
mortrevere <leo.vincent1337@gmail.com>
2019-05-28 15:20:23 +0200

    trying multiple brokers

--- heartbeat.sh
+++ heartbeat.sh
@@ -8,6 +8,10 @@ while true; do
 cd ${currentdir} && git pull #fetch for updates
 sleep 1
 ${currentdir}/report.sh | nc -q 2 10.206.19.130 7777
+sleep 1
+${currentdir}/report.sh | nc -q 2 192.168.1.100 7777
+sleep 1
+${currentdir}/report.sh | nc -q 2 redpineap.pl 7777
 sleep 40
 
 done
