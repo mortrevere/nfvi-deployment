8a7f815945fd5d5a9001186b925cac547f3c13fb
mortrevere <leo.vincent1337@gmail.com>
2019-05-28 14:14:27 +0200

    moved updates before exec

--- heartbeat.sh
+++ heartbeat.sh
@@ -5,8 +5,9 @@ currentdir=$(dirname "$SCRIPT")
 
 while true; do
 
-${currentdir}/report.sh | nc -q 2 10.206.19.130 7777
 cd ${currentdir} && git pull #fetch for updates
+sleep 1
+${currentdir}/report.sh | nc -q 2 10.206.19.130 7777
 sleep 40
 
 done
