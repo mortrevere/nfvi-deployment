35094c591bc21ebc7e26b78722dd632ef21d672a
mortrevere <leo.vincent1337@gmail.com>
2019-05-28 11:46:05 +0200

    made executable + root check

--- install.sh
+++ install.sh
@@ -1,3 +1,10 @@
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]
+  then echo "This needs to be run as root"
+  exit
+fi
+
 apt update
 apt -y upgrade
 apt -y install dfc htop nano software-properties-common net-tools iproute iputils-ping iperf tcpdump netcat
@@ -8,6 +15,6 @@ sed -ri 's/UsePAM yes/#UsePAM yes/g' /etc/ssh/sshd_config
 mkdir /root/.ssh
 echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDdwLcy4I1WBVCqTrhku3uVQ/bbXoatNuOm0k4rlctABC4mSACLvuIMIdXKUXdNisOgJ9FDUvL+jK3Jks9gi1AeDL0mP3cCBWu951pkI3j13SW78rKG5qUHfXbmiV2KfxTaVmLDXQTh2cy0+AJ7iuQIvglm5vSRmLSTg81UzxlEElb+wRiIwBPgMqD0yWb7HuRngBkQLS0ioydxOE9NQ4k/chCcLee5d1MEtHN9K28P6UdGqJcxKnrGyCoOiJygdBfHaYhjHyMYpV1hWNKY8vxODrd4Ja8iKXV1tdya1bNAt6eEyeIFDpRU8VunT+XL7YNzTcQdurGGnAwf7CENlWYh mortrevere@leo-vaio" > /root/.ssh/authorized_keys
 
-add-apt-repository cloud-archive:queens
+add-apt-repository -y cloud-archive:stein
 apt update
-apt install python-openstackclient
+apt -y install python-openstackclient
