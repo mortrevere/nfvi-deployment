d96a87d3524ebfb14d2c3bc4f1663fe52621a368
mortrevere <leo.vincent1337@gmail.com>
2019-06-03 15:12:22 +0200

    added ram stats

--- report.sh
+++ report.sh
@@ -5,5 +5,6 @@ df -h | grep sda
 date +%R:%S
 uptime
 ping -w 1 -c 1 8.8.8.8 > /dev/null && echo 'internet up' || echo 'internet down'
+free | grep Mem
 echo 'LANPING'
 cat /etc/hosts | cut -f1 | grep -Ev '#|127.0.' | xargs -L 1 ping -c1 -w1 2> /dev/null | grep -A 1 PING
