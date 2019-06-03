80a9d2be66b4f710ca2c530a476348ee1bc79aa9
mortrevere <leo.vincent1337@gmail.com>
2019-06-03 12:12:55 +0200

    removed localhost from ping tests

--- report.sh
+++ report.sh
@@ -6,4 +6,4 @@ date +%R:%S
 uptime
 ping -w 1 -c 1 8.8.8.8 > /dev/null && echo 'internet up' || echo 'internet down'
 echo 'LANPING'
-cat /etc/hosts | cut -f1 | grep -v '#' | xargs -L 1 ping -c1 -w1 2> /dev/null | grep -A 1 PING
+cat /etc/hosts | cut -f1 | grep -Ev '#|127.0.' | xargs -L 1 ping -c1 -w1 2> /dev/null | grep -A 1 PING
