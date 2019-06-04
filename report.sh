1a8d03f10f5592b1935358884c654582872de099
mortrevere <leo.vincent1337@gmail.com>
2019-06-04 15:27:02 +0200

    cleaner reporting

--- report.sh
+++ report.sh
@@ -8,7 +8,15 @@ ping -w 1 -c 1 8.8.8.8 > /dev/null && echo 'internet up' || echo 'internet down'
 free | grep Mem
 echo 'LANPING'
 cat /etc/hosts | cut -f1 | grep -Ev '#|127.0.' | xargs -L 1 ping -c1 -w1 2> /dev/null | grep -A 1 PING
-echo 'PROCESSES'
-ps --no-headers -ww -eo pcpu,pid,user,comm | sort -nrk 1 | head -10
-echo 'WEBSERVICES'
-netstat -lantp | grep 'LISTEN.*apache2' | awk '{print $4 " " $7}'
+
+processes=$(ps --no-headers -ww -eo pcpu,pid,user,comm | sort -nrk 1 | head -10)
+processesc=$(echo "$processes" | wc -l)
+echo "PROCESSES ${processesc}"
+echo "$processes"
+
+
+ws=$(netstat -lantp | grep 'LISTEN.*apache2' | awk '{print $4 " " $7}')
+wsc=$(echo "$ws" | wc -l)
+echo "WEBSERVICES ${wsc}"
+echo "$ws"
+
