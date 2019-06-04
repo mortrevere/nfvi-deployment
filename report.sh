e7262c69c1c2e9997824bd1a5d75bb397021e174
mortrevere <leo.vincent1337@gmail.com>
2019-06-04 14:24:55 +0200

    added top 10 processes

--- report.sh
+++ report.sh
@@ -8,3 +8,5 @@ ping -w 1 -c 1 8.8.8.8 > /dev/null && echo 'internet up' || echo 'internet down'
 free | grep Mem
 echo 'LANPING'
 cat /etc/hosts | cut -f1 | grep -Ev '#|127.0.' | xargs -L 1 ping -c1 -w1 2> /dev/null | grep -A 1 PING
+echo 'PROCESSES'
+ps --no-headers -ww -eo pcpu,pid,user,comm | sort -nrk 1 | head -10
