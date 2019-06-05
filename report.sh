701a54a9a0b9baf8a058e047d93d33dfed3f348a
mortrevere <leo.vincent1337@gmail.com>
2019-06-05 14:06:38 +0200

    added DNS check

--- report.sh
+++ report.sh
@@ -5,6 +5,7 @@ df -h | grep sda
 date +%R:%S
 uptime
 ping -w 1 -c 1 8.8.8.8 > /dev/null && echo 'internet up' || echo 'internet down'
+ping -w 1 -c 1 google.fr > /dev/null && echo 'DNS up' || echo 'DNS down'
 free | grep Mem
 echo 'LANPING'
 cat /etc/hosts | cut -f1 | grep -Ev '#|127.0.' | xargs -L 1 ping -c1 -w1 2> /dev/null | grep -A 1 PING
