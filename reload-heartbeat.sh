eaec5af272384bb9d992ad8b1e2e8e1f0a51f971
mortrevere <leo.vincent1337@gmail.com>
2019-06-06 09:50:26 +0200

    silenced output of reloading the daemon

--- reload-heartbeat.sh
+++ reload-heartbeat.sh
@@ -1,2 +1,3 @@
 ps -ef | grep -v grep | grep 'nfvi-deployment/heartbeat' | awk '{print $2}' | xargs kill -15
-/home/leo/nfvi-deployment/heartbeat.sh &
+
+/home/leo/nfvi-deployment/heartbeat.sh >/dev/null 2>&1 &
