ea0c2b66b54cffc87a308caf560d81b050fe323d
mortrevere <leo.vincent1337@gmail.com>
2019-06-11 16:59:03 +0200

    hosts for shepherd

--- heartbeat.sh
+++ heartbeat.sh
@@ -8,7 +8,7 @@ while true; do
 
 cd ${currentdir} && git pull #fetch for updates
 sleep 1
-${currentdir}/report.sh | nc -q 2 192.168.1.79 7777
+${currentdir}/report.sh | nc -q 2 leo-vaio 7777
 #sleep 1
 #${currentdir}/report.sh | nc -q 2 redpineap.pl 7777
 sleep 8
