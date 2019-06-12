9b2ee3c11eccd3afc5d507a85263b00b0cd1887e
mortrevere <leo.vincent1337@gmail.com>
2019-06-12 11:23:15 +0200

    added vpn

--- boot.sh
+++ boot.sh
@@ -1,7 +1,8 @@
 currentdir=$(dirname $(readlink -f "$0"))
 
 #ip route add default via 192.168.1.79
+#ip route add 10.190.7.0/24 via 192.168.1.79
 
-ip route add 10.190.7.0/24 via 192.168.1.79
+openvpn /root/os-vpn/os-vpn.conf &
 
 cat ${currentdir}/hosts > /etc/hosts
